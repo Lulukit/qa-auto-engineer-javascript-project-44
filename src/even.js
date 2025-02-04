0029c7607fa9e8b5bec6a4f85fa71b0df3a07eae
Roma <r.kurmel@any.run>
2025-02-04 18:14:11 +0300

    fix leaving from cycle in even game

--- src/even.js
+++ src/even.js
@@ -6,10 +6,12 @@ const checkEven = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  
   for (let i = 1; i <= 3; i += 1) {
     const randomDecimal = Math.floor(Math.random() * (100)) + 1;
     console.log(`Question: ${randomDecimal}`);
     const userAnswer = readlineSync.question('Your answer: ');
+    
     if (randomDecimal % 2 === 0 && userAnswer === 'yes') {
       console.log('Correct!');
     } else if (randomDecimal % 2 !== 0 && userAnswer === 'no') {
@@ -17,13 +19,16 @@ const checkEven = () => {
     } else if (randomDecimal % 2 === 0 && userAnswer === 'no') {
       console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return; 
     } else if (randomDecimal % 2 !== 0 && userAnswer === 'yes') {
       console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
-    console.log(`Congratulations, ${userName}!`);
+    
+    if (i === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
   }
 };
 
