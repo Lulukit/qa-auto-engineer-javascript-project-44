41c9181be67c25ad28212c8a2d7e1c69d3460ddc
Roma <r.kurmel@any.run>
2025-02-04 17:50:40 +0300

    one more fix

--- src/even.js
+++ src/even.js
@@ -4,7 +4,7 @@ import * as readlineSync from 'readline-sync';
 const checkEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     const randomDecimal = Math.floor(Math.random() * (100)) + 1;
