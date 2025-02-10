22b5c5cb581dee6e6f7f2a9784ef3b0b2c1dfb89
Roma <r.kurmel@any.run>
2025-02-10 15:53:42 +0300

    fix comments

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,3 @@
-export const description = 'What is the result of the expression?';
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getRandomOperation = () => {
@@ -6,30 +5,28 @@ const getRandomOperation = () => {
   return operations[Math.floor(Math.random() * operations.length)];
 };
 
+const calculateExpression = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unexpected operation: ${operation}`);
+  }
+};
+
+export const description = 'What is the result of the expression?';
+
 export const generateQuestion = () => {
   const num1 = getRandomNumber(1, 25);
   const num2 = getRandomNumber(1, 25);
   const operation = getRandomOperation();
 
-  let question; let
-    answer;
-
-  switch (operation) {
-    case '+':
-      question = `${num1} + ${num2}`;
-      answer = (num1 + num2).toString();
-      break;
-    case '-':
-      question = `${num1} - ${num2}`;
-      answer = (num1 - num2).toString();
-      break;
-    case '*':
-      question = `${num1} * ${num2}`;
-      answer = (num1 * num2).toString();
-      break;
-    default:
-      throw new Error(`Unexpected operation: ${operation}`);
-  }
+  const question = `${num1} ${operation} ${num2}`;
+  const answer = calculateExpression(num1, num2, operation).toString();
 
   return { question, answer };
 };
